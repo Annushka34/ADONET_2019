d390312e4e71d6dfe8afcdee3c88e24d03e27d45
annushka34 <annsamoluk82@gmail.com>
2019-06-29 16:31:48 +0300

    Lesson 1

---
+++ 01_Lesson/01_Lesson/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // string connectionString = @"Data Source=DESKTOP-LNTJUIU;Initial Catalog=BookShopPublisher;User ID=ANNA;Password=12;";
+           // string connectionString = @"Data Source=DESKTOP-LNTJUIU;Initial Catalog=BookShopPublisher;Integrated Security=true;";
+            SqlConnection connection = new SqlConnection();
+            try
+            {
+               // connection.ConnectionString = connectionString;
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+                connection.Open();
+                Console.WriteLine("Hello. Im connected!");
+                //var id = Console.ReadLine();
+                //string query = "select BookName From Book Where Id=" + id;
+                //SqlCommand command = new SqlCommand(query, connection);
+                //var rezult =  command.ExecuteScalar();//---HERE QUERY DONE!!!
+                //Console.WriteLine(rezult);
+
+                //Console.WriteLine("Inserted - ");
+                //Console.WriteLine(InsertIntoAutor(connection));
+
+                //SelectAllFromAutor(connection);
+                //SelectAllFromBook(connection);
+                SelectAllFrom(connection);
+                connection.Close();
+                Console.WriteLine(  "You select:  "+ Menu());
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Connection close in catch block");
+            }
+            finally
+            {
+                connection.Close();
+                Console.WriteLine("Connection close");
+            }
+        }
+
+        static int InsertIntoAutor(SqlConnection con)
+        {
+            string query = @"INSERT INTO Author VALUES('Петров',3,2);";
+            SqlCommand com = new SqlCommand(query, con);
+            //---FOR DELETE UPDATE INSERT---
+            var rez = com.ExecuteNonQuery();
+            return rez;
+        }
+
+        static int SelectAllFromAutor(SqlConnection con)
+        {
+            string query = @"SELECT * FROM Author";
+            SqlCommand com = new SqlCommand(query, con);
+            SqlDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0] + " - " + reader[1] + " - " + reader["PublishId"]);
+            }
+            return 0;
+        }
+
+        static int SelectAllFromBook(SqlConnection con)
+        {
+            string query = @"SELECT * FROM Book";
+            SqlCommand com = new SqlCommand(query, con);
+            SqlDataReader reader = com.ExecuteReader();
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                Console.Write($"{reader.GetName(i), 15}");
+            }
+            Console.WriteLine();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader[i],15}");
+                }
+                Console.WriteLine();
+            }
+           
+            return 0;
+        }
+
+        static int SelectAllFrom(SqlConnection con)
+        {
+            string query = @"SELECT * FROM Book; SELECT * FROM Author;";
+            SqlCommand com = new SqlCommand(query, con);
+            SqlDataReader reader = com.ExecuteReader();
+            int counter = 0;
+            do
+            {
+                while (reader.Read())
+                {
+                    if (counter == 0)
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write($"{reader.GetName(i),15}");
+                        }
+                    }
+                    Console.WriteLine();
+                    counter++;
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        Console.Write($"{reader[i],15}");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("-----------------------------------------------");
+                counter = 0;
+            }
+            while (reader.NextResult());
+            return 0;
+        }
+
+        static int Menu()
+        {
+            int selected = 0;
+            string[] menu = new string[4]
+            {
+                "1. Create",
+                "2. Read",
+                "3. Update",
+                "4. Delete"
+            };
+            while (true)
+            {
+                for (int i = 0; i < menu.Length; i++)
+                {
+                    if (i == selected)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("->");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                    Console.WriteLine(menu[i]);
+                    Console.ResetColor();
+                }
+               switch( Console.ReadKey().Key)
+               {
+                    case ConsoleKey.DownArrow:
+                        {
+                            selected = selected < menu.Length - 1 ? selected + 1 : 0;
+                            Console.Clear();
+                            break;
+                        }
+                    case ConsoleKey.Enter:
+                        {
+                            return selected;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ 01_Lesson/02_Parametrized/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Parametrized
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                // connection.ConnectionString = connectionString;
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+                connection.Open();
+                string query = "Select BookName From Book where Id = @a";
+
+                SqlCommand com = new SqlCommand(query, connection);
+                Console.WriteLine("Enter Id Book");
+                string str = Console.ReadLine();
+                int.TryParse(str, out var Id);
+                SqlParameter param = new SqlParameter("@a", System.Data.SqlDbType.Int);
+                param.Value = Id;
+                com.Parameters.Add(param);
+
+                string res = (string)com.ExecuteScalar();
+                Console.WriteLine(res);
+
+               
+                string sqlProcedName = "GetBookAuthorAddress";
+                SqlCommand com1 = new SqlCommand(sqlProcedName, connection);
+                com1.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlDataReader reader =  com1.ExecuteReader();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader.GetName(i), 15}");
+                }
+                Console.WriteLine();
+                while(reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        Console.Write($"{reader[i],15}");
+                    }
+                    Console.WriteLine();
+                }
+                reader.Close();
+
+
+                //---PROCEDURE WITH PARAMETERS INPUT AND OUTPUT---
+                string sqlProcedName1 = "InsertIntoBook";
+                SqlCommand com2 = new SqlCommand(sqlProcedName1, connection);
+                com2.CommandType = System.Data.CommandType.StoredProcedure;
+                //com2.Parameters.AddWithValue("@name", "Как правильно мечтать");
+                SqlParameter param2 = new SqlParameter("@name",System.Data.SqlDbType.NVarChar, 250);
+                param2.Value = "Мистецтво війни";
+                com2.Parameters.Add(param2);
+                SqlParameter param3 = new SqlParameter("@categoryName", System.Data.SqlDbType.NVarChar, 250);
+                param3.Value = "Історичні";
+                com2.Parameters.Add(param3);
+                SqlParameter param4= new SqlParameter("@author", System.Data.SqlDbType.NVarChar, 250);
+                param4.Value = "Петров";
+                com2.Parameters.Add(param4);
+                SqlParameter param5 = new SqlParameter("@price", System.Data.SqlDbType.Money);
+                param5.Value = 250;
+                com2.Parameters.Add(param5);
+                SqlParameter param6 = new SqlParameter("@page", System.Data.SqlDbType.Int);
+                param6.Value = 199;
+                com2.Parameters.Add(param6);
+
+                SqlParameter param7 = new SqlParameter("@succesMsg", System.Data.SqlDbType.NVarChar, 250);
+                param7.Direction = System.Data.ParameterDirection.Output;
+                com2.Parameters.Add(param7);
+                SqlParameter param8 = new SqlParameter("@amountRows", System.Data.SqlDbType.Int);
+                param8.Direction = System.Data.ParameterDirection.Output;
+                com2.Parameters.Add(param8);
+
+                com2.ExecuteNonQuery();
+                Console.WriteLine(com2.Parameters["@succesMsg"].Value.ToString());
+            }
+        }
+    }
+}
